1fee6349ecd9fe64603437f3107e50b4121235e2
PhamPhien-gif <toilaphien2302@gmail.com>
2025-06-09 15:43:08 +0700

    done

--- AppQuanLyKho/src/appquanlykho/AppQuanLyKho.java
+++ AppQuanLyKho/src/appquanlykho/AppQuanLyKho.java
@@ -1,20 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package appquanlykho;
 
-/**
- *
- * @author HP
- */
+import appquanlykho.GUI.LOGIN;
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.UIManager;
+
 public class AppQuanLyKho {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception ex) {
+            System.err.println("Không thể cài đặt FlatLaf");
+        }
+         LOGIN login = new LOGIN();
+         login.setVisible(true);    
+    
     }
     
 }
